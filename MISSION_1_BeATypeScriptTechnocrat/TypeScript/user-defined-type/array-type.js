d3e3ece06aab02ec764769d519fac4171049dc39
AsifHossain22 <only1asif4you22@gmail.com>
2026-04-21 12:40:38 +0400

    Explored 'Array Types'

---
+++ MISSION_1_BeATypeScriptTechnocrat/TypeScript/user-defined-type/array-type.js
@@ -0,0 +1,8 @@
+"use strict";
+let userNames;
+userNames = ["asif", "akashi", "ratul", "johora"];
+console.log(userNames[0]);
+userNames.sort();
+console.log(userNames);
+let multipleTypes;
+multipleTypes = [1, "ASIF"];
---
+++ MISSION_1_BeATypeScriptTechnocrat/TypeScript/user-defined-type/array-type.ts
@@ -0,0 +1,11 @@
+let userNames: string[];
+
+userNames = ["asif", "akashi", "ratul", "johora"];
+
+console.log(userNames[0]);
+
+userNames.sort();
+console.log(userNames);
+
+let multipleTypes: (string | number)[];
+multipleTypes = [1, "ASIF"];
