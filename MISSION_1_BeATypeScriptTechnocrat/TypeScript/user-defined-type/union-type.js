d89db5b4cb2475864f2c5491b0bb1fc456ea7d0a
AsifHossain22 <only1asif4you22@gmail.com>
2026-04-21 11:46:51 +0400

    Explored

---
+++ MISSION_1_BeATypeScriptTechnocrat/TypeScript/user-defined-type/union-type.js
@@ -0,0 +1,10 @@
+"use strict";
+let useId;
+useId = "101";
+useId = 101;
+useId = true;
+function displayUserInfo(useId) {
+    console.log(useId);
+}
+displayUserInfo("101");
+displayUserInfo(101);
---
+++ MISSION_1_BeATypeScriptTechnocrat/TypeScript/user-defined-type/union-type.ts
@@ -0,0 +1,11 @@
+let useId: string | number | boolean;
+
+useId = "101";
+useId = 101;
+useId = true;
+
+function displayUserInfo(useId: string | number) {
+  console.log(useId);
+}
+displayUserInfo("101");
+displayUserInfo(101);
