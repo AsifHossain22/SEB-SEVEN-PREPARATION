ee4ee39c46030135fae4e89434b078c3ba4475a0
AsifHossain22 <only1asif4you22@gmail.com>
2026-04-21 15:06:46 +0400

    Explored 'Tuple Type'

---
+++ MISSION_1_BeATypeScriptTechnocrat/TypeScript/user-defined-type/tuple-type.js
@@ -0,0 +1,6 @@
+"use strict";
+// MixedData - key, value
+let user;
+user = [101, "ASIF"];
+user.push(102, "JOHORA");
+console.log(user);
---
+++ MISSION_1_BeATypeScriptTechnocrat/TypeScript/user-defined-type/tuple-type.ts
@@ -0,0 +1,8 @@
+// MixedData - key, value
+
+let user: [number, string];
+user = [101, "ASIF"];
+
+user.push(102, "JOHORA");
+
+console.log(user);
