1ac5f167697378a9eb16dfac560a953b5af79b4c
AsifHossain22 <only1asif4you22@gmail.com>
2026-04-27 11:31:20 +0400

    Next.js set up

---
+++ Mission 4 - Be a Prisma - Next.js Ninja/Next Crash/next-crash/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="mt-10">
+      <div>
+        lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas,
+        dicta
+      </div>
+    </main>
+  );
+}
